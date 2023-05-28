c48db7a4cc30163457364105a530cfda7a40b3c6
Konstantin Surovskiy <git.user.wollter@mail.ru>
2023-05-28 18:17:35 +0300

    Задание по курсовой Циклы.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
+import java.io.File;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int fileCount = 0;
+
+            while (true) {
+                System.out.println("Please enter the path to the file: ");
+                String filePath = sc.nextLine();
+
+                File file = new File(filePath);
+
+                boolean fileExists = file.exists();
+                boolean isFile = file.isFile();
+
+                if (!fileExists || !isFile) {
+                    System.out.println("The specified file does not exist or the path is a directory, not a file. Please try again.");
+                    continue;
+                } else {
+                    fileCount++;
+                    System.out.println("The path is correct. This is file number " + fileCount);
+                }
+            }
+        }
     }
-}
+
